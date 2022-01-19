5262dafd8f72c2417a1b6e488d206f0f3171b968
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2022-01-19 21:04:35 +0200

    clasa pentru criptarea parolei

---
+++ Banca_v5/Hash.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Security.Cryptography;
+using System.Threading.Tasks;
+
+namespace Banca_v5
+{
+    class Hash
+    {
+        public static string Hash_SHA1(string input)
+        {
+            using (SHA1Managed sha1 = new SHA1Managed())
+            {
+                var hash = sha1.ComputeHash(Encoding.UTF8.GetBytes(input));
+                var sb = new StringBuilder(hash.Length * 2);
+
+                foreach (byte b in hash)
+                {
+                    sb.Append(b.ToString("X2"));
+                }
+
+                return sb.ToString();
+            }
+        }
+    }
+}
+
+
+
+
