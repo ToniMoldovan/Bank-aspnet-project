b3c276dc843d4747c44c46602253469a993df32c
IancuMilu <iancu.milu@gmail.com>
2022-01-19 20:30:16 +0200

    Clase => Models

---
+++ Banca_v5/Models/ContBancar.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity; // mostenire DbContext
+using System.ComponentModel.DataAnnotations; // adnotari
+
+namespace Banca_v5.Models
+{
+    public class ContBancar
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int IdPersoana { get; set; }
+
+        public string IBAN { get; set; }
+
+        public int Pin { get; set; }
+
+        public double Suma { get; set; }
+
+        public string Moneda { get; set; }
+    }
+
+    public class ContBancarDbContext : DbContext
+    {
+        public DbSet<ContBancar> ConturiBancare { get; set; }
+    }
+}
---
+++ Banca_v5/Models/Utilizator.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using System.Data.Entity; // mostenire DbContext
+using System.ComponentModel.DataAnnotations; // adnotari
+
+namespace Banca_v5
+{
+    public class Utilizator
+    {
+        [Key]
+        public int Id { get; set; }
+
+        public int IdCont { get; set; }
+
+        public string Nume { get; set; }
+
+        public string Prenume { get; set; }
+
+        public string Email { get; set; }
+
+        public string UserName { get; set; }
+
+        public string Parola { get; set; }        
+    }
+
+    public class UtilizatorDbContext : DbContext
+    {
+        public DbSet<Utilizator> Utilizatori { get; set; }
+    }
+}
