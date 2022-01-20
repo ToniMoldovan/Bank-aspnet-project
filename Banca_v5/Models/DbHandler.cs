f5bf6565bbe0e8ffd14f2524153b34fca13bbfdf
Toni <antonio.moldovan82@yahoo.com>
2022-01-20 09:18:41 +0200

    added function dbhandler

--- Banca_v5/Models/DbHandler.cs
+++ Banca_v5/Models/DbHandler.cs
@@ -128,6 +128,25 @@ namespace Banca_v5.Models
             return true;
         }
 
+        /*Adaugare cont bancar - TONI MODIFICA AICI*/
+        public bool AdaugareContBancar(int idPers, string IBAN, int pin, double suma, string moneda)
+        {
+            var existaDeja = (from c in cntBnc.ConturiBancare
+                              where c.IdPersoana == idPers
+                              select c).ToList();
+
+            foreach (var item in existaDeja)
+            {
+                if (item.Moneda == moneda)
+                {
+                    // nu mai paote
+                    return false;
+                }
+            }
+
+            return true;
+        }
+
         public DataGridView ActualizareClienti()
         {
             try
