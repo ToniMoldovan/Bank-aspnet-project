0897d321021369dd514974fd141cdfff22252595
ionutnoje <65008544+ionutnoje@users.noreply.github.com>
2022-01-20 10:34:26 +0200

    schimbari la language

--- Banca_v5/Views/AfisareClientiForm.Designer.cs
+++ Banca_v5/Views/AfisareClientiForm.Designer.cs
@@ -37,40 +37,25 @@ namespace Banca_v5.Views
             // 
             // label1
             // 
-            this.label1.AutoSize = true;
-            this.label1.Font = new System.Drawing.Font("Gadugi", 10.8F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(32, 48);
+            resources.ApplyResources(this.label1, "label1");
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(171, 21);
-            this.label1.TabIndex = 0;
-            this.label1.Text = "Toti clientii bancii:";
             // 
             // dgvClienti
             // 
+            resources.ApplyResources(this.dgvClienti, "dgvClienti");
             this.dgvClienti.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.AutoSize;
-            this.dgvClienti.Location = new System.Drawing.Point(32, 96);
             this.dgvClienti.Name = "dgvClienti";
-            this.dgvClienti.RowHeadersWidth = 51;
             this.dgvClienti.RowTemplate.Height = 24;
-            this.dgvClienti.Size = new System.Drawing.Size(816, 416);
-            this.dgvClienti.TabIndex = 1;
             // 
             // AfisareClientiForm
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(10F, 21F);
+            resources.ApplyResources(this, "$this");
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(882, 553);
             this.Controls.Add(this.dgvClienti);
             this.Controls.Add(this.label1);
-            this.Font = new System.Drawing.Font("Gadugi", 10.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
             this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
-            this.Icon = ((System.Drawing.Icon)(resources.GetObject("$this.Icon")));
-            this.ImeMode = System.Windows.Forms.ImeMode.NoControl;
-            this.Margin = new System.Windows.Forms.Padding(4, 4, 4, 4);
             this.MaximizeBox = false;
             this.Name = "AfisareClientiForm";
-            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
-            this.Text = "Clienti";
             ((System.ComponentModel.ISupportInitialize)(this.dgvClienti)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
